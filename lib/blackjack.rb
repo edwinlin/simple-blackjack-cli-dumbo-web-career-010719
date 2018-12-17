8fcb1e64393b465665378512f7dc0ce443a593c5
Edwin Lin <edwinlin@gmail.com>
2018-12-17 05:04:33 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -70,10 +70,11 @@ def runner
   while card_total < 21
     card_total = hit?(card_total)
     display_card_total(card_total)
+        end_game(card_total)
+
   end
   
   # if card_total > 21
-    end_game(card_total)
   # end
 end
 
